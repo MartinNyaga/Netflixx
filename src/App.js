8caf252fb5750de79eb7c0c475e094908cbca6f6
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-08-01 08:54:16 +0300

    movie list from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import './App.css';
+import Header from "./Components/Header";
+import MovieList from "./Components/MovieList";
+import BackGroundImage from "./Assets/home.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <container>
+      <Router>
+        <div className="app">
+          <Header />
+        </div>
+        <Routes>
+          <Route exact path="/" Component={MovieList}/>
+        </Routes>
+      </Router>
+      </container>
   );
 }
 
