b1844d111501381de5d43f4df27e767784d70722
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-08-01 09:25:49 +0300

    remove background image and container

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import './App.css';
 import Header from "./Components/Header";
 import MovieList from "./Components/MovieList";
-import BackGroundImage from "./Assets/home.jpg"
 
 function App() {
   return (
-      <container>
       <Router>
         <div className="app">
           <Header />
@@ -16,7 +14,6 @@ function App() {
           <Route exact path="/" Component={MovieList}/>
         </Routes>
       </Router>
-      </container>
   );
 }
 
