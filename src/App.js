f0ba610ae1fd3cd3e4b07ec4dfca8a31b627d69b
Misra2023 <Mishazabdi@gmail.com>
2023-08-01 09:54:11 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-
+import React from 'react';
 import './App.css';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <h1>Netflixx work on going</h1>
       </header>
+      {/* Your main content goes here */}
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
