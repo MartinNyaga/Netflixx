106748a566f13c16000c2dbbc39dd032ee3e73c9
Njeri Mugeshi <nicolemugeshi@gmail.com>
2023-08-03 21:07:09 +0300

    removed un used variables

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const MovieList = ({ movies }) => {
 
@@ -38,7 +38,7 @@ const MovieList = ({ movies }) => {
         };
 
         fetchMovieDetails();
-    }, [movies]);
+    }, [moviess]);
 
     const handleMouseEnter = (movieId) => {
         setIsHovering(movieId);
